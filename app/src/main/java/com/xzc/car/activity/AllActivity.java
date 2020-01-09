f8d01928ba65dd38a0af0c1475a8489e0969a801
刘道辉 <1462926548@qq.com>
2020-01-10 02:56:22 +0800

    ldh

---
+++ app/src/main/java/com/xzc/car/activity/AllActivity.java
@@ -0,0 +1,14 @@
+package com.xzc.car.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.xzc.car.R;
+import android.os.Bundle;
+//全部预约信息
+public class AllActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all);
+    }
+}
---
+++ app/src/main/java/com/xzc/car/activity/OverActivity.java
@@ -0,0 +1,14 @@
+package com.xzc.car.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.xzc.car.R;
+import android.os.Bundle;
+
+public class OverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_over);
+    }
+}
