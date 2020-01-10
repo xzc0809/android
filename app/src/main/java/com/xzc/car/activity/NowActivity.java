0aca4b7bfbf18a248a7a43a29dd390a647e63a40
cr7hibiki <you@example.com>
2020-01-10 09:41:17 +0800

    Merge branch 'master' of https://github.com/xzc0809/android
    # Conflicts:
    #	app/src/main/java/com/xzc/car/activity/MyAppointmentActivity.java

--- app/src/main/java/com/xzc/car/activity/NowActivity.java
+++ app/src/main/java/com/xzc/car/activity/NowActivity.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class NowActivity extends AppCompatActivity {
     private Button btn_mine,btn_all,btn_now,btn_over;
+    private ListView lv2_all;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +42,6 @@ public class NowActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        lv2_all=findViewById(R.id.lv2_all);
     }
 }
